418d96ba3cd3e14582b2d39ad71157b781d0e8a6
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-05-05 14:34:01 +0100

    Update MainPublisherOne.java

--- distsys/src/main/java/org/taba/question4/Publisher1/MainPublisherOne.java
+++ distsys/src/main/java/org/taba/question4/Publisher1/MainPublisherOne.java
@@ -1,3 +1,17 @@
+/**
+ * National College Ireland - NCI
+ * Terminal Based Assignment Assessment - TABA
+ * HD Computer Science - International
+ *              ---
+ *        Distributed System
+ *      Lecturer: Mark Cudden
+ *              ---
+ * Author: Sergio Vinicio da Silva Oliveira
+ * x23170981@student.ncirl.ie
+ *              ---
+ * Question 4:By using the MQTT protocol
+ * implement in Java the Publisher - Subscriber
+ */
 package org.taba.question4.Publisher1;
 
 import org.taba.question4.Publisher2.PublisherTwoLights;
