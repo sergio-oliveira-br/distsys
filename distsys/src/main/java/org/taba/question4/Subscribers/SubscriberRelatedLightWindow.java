94e4e9711f41fd4c5b7b5bcd85f1e3b251ad4c88
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-05-06 09:58:22 +0100

    Update SubscriberRelatedLightWindow.java
    all functionalities implemented

--- distsys/src/main/java/org/taba/question4/Subscribers/SubscriberRelatedLightWindow.java
+++ distsys/src/main/java/org/taba/question4/Subscribers/SubscriberRelatedLightWindow.java
@@ -14,5 +14,72 @@
  */
 package org.taba.question4.Subscribers;
 
-public class SubscriberRelatedLightWindow {
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * This Subscriber listens for messages:
+ * 4.d) messages that are related to the ALL FLOORS for both light and window
+ */
+public class SubscriberRelatedLightWindow
+{
+    /** Instance Variables */
+    //From Publisher 2, Lights:
+    private static final String QUEUE_LIGHTS_KITCHEN = "1ºFloor/Lights:Kitchen/ID:1122";
+    private static final String QUEUE_LIGHTS_ROOM = "2ºFloor/Light:Room/ID:2254";
+    private static final String QUEUE_LIGHTS_OFFICE = "2ºFloor/Light:Office/ID:2260";
+    private static final String QUEUE_LIGHTS_LOUNGE = "1ºFloor/Light:Lounge/ID:1197";
+
+    //From Publisher 2, Window:
+
+
+
+
+    private static final CountDownLatch latch = new CountDownLatch(1);
+
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");   //RabbitMQ server host -> http://localhost:15672/#/
+
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        /** Declare the Queues*/
+        //From Publisher 2, Lights:
+        channel.queueDeclare(QUEUE_LIGHTS_KITCHEN, true, false, false, null);
+        channel.queueDeclare(QUEUE_LIGHTS_ROOM, true, false, false, null);
+        channel.queueDeclare(QUEUE_LIGHTS_OFFICE, true, false, false, null);
+        channel.queueDeclare(QUEUE_LIGHTS_LOUNGE, true, false, false, null);
+
+        //From Publisher 2, Window:
+
+
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        //Create a consumer and bind it to the queues
+        Consumer consumer = new DefaultConsumer(channel)
+        {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+            }
+        };
+
+        /**Start consuming messages*/
+        //From Publisher 2, Lights:
+        channel.basicConsume(QUEUE_LIGHTS_KITCHEN, true, consumer);
+        channel.basicConsume(QUEUE_LIGHTS_ROOM, true, consumer);
+        channel.basicConsume(QUEUE_LIGHTS_OFFICE, true, consumer);
+        channel.basicConsume(QUEUE_LIGHTS_LOUNGE, true, consumer);
+
+        //From Publisher 2, Window:
+
+        /**Wait indefinitely until notified to exit */
+        latch.await();
+    }
 }
